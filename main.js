3f48d7847490e67f757d259f526025f87ad966a9
Rachel Kelly <racheldk126@gmail.com>
2022-11-16 15:22:17 -0800

    v-for examples

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ const app = Vue.createApp({
             url: 'http://placekitten.com/200/300',
             inventory: 100,
             onSale: false,
+            details: ['50% cotton', '30% wool', '20% polyester'], 
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'},
+            ],
+            sizes: ['small', 'medium', 'large'],
         }
     }
 })
